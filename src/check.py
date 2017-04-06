bf45ab0d884e9f07e4ec00d6d9bcf66e193c255c
Jack Kellner <jackkellner@Jacks-MacBook-Pro.local>
2017-04-06 13:01:07 -0700

    Initial pass at running .smv file in terminal

--- src/check.py
+++ src/check.py
@@ -1,4 +1,42 @@
+import subprocess
+from pprint import pprint
+
 class Check(object):
-    def __init__(self, data=''):
-        """Initialize a check object"""
-        self.data = data
+	def __init__(self, data=''):
+		"""Initialize a check object"""
+		self.data = data
+		
+
+class Checks(object):
+	def __init__(self):
+		self.checks = []
+
+	def add_check(check):
+		self.checks.append(check)
+
+	def compile(self):
+		# function to compile a .smv file from the list of checks 
+		return 'tests/waiter_customer_example.smv'
+
+	def run(self):
+		# Return the name of the .smv file to run in terminal
+		file = self.compile()
+
+		# Initialize an array to hold the results of the checks
+		self.results = []
+
+		# create the command and run in terminal
+		self.output = subprocess.check_output(['NuSMV', file]).splitlines()
+		# retain only the lines that return whether or not a specification is true/false
+		self.output = [x for x in self.output if x[:16] == '-- specification']
+
+		# Iterate throught the results and parse whether or not a statement is valid
+		for i in range(len(self.output)):
+			if 'is false' in self.output[i]:
+				self.results.append(True)
+			elif 'is true' in output[i]:
+				self.results.append(False)
+
+		# print output to console
+		pprint(self.output)
+		pprint(self.results)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,19 @@
 import os, sys, unittest
 sys.path.append(os.path.join(os.getcwd(), os.path.pardir))
 from src.parser import Parser
+from src.check import Checks
+from src.check import Check
 
 class TestParser(unittest.TestCase):
 
     def test_parser(self):
         parser_obj = Parser()
         parser_obj.parse('./tests/waiter_customer.txt')
+
+    def test_checks(self):
+    	checks = Checks()
+    	checks.run()
+
 
 
 
